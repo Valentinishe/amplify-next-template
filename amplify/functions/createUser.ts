99b158d6b9cb0faa87d92d75ab5aa8c42d2d34d0
Valentyn <valentynishe.gmail.com>
2024-07-23 15:13:34 +0300

    aaa

--- amplify/functions/createUser.ts
+++ amplify/functions/createUser.ts
@@ -1,4 +1,9 @@
 export const handler = async (event: any) => {
+  console.log('body', event?.body);
+  console.log('query', event?.query);
+  console.log('params', event?.params);
+  console.log('event', event);
+
   return {
     statusCode: 200,
     body: JSON.stringify(event),
