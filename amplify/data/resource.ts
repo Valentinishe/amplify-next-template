1ef6df3ff750b3fb318b5fd3c6dd234efb869d6d
Valentyn <valentynishe.gmail.com>
2024-07-23 11:03:15 +0300

    aa

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,8 +9,7 @@ specifies that any user authenticated via an API key can "create", "read",
 const schema = a.schema({
   Todo: a
     .model({
-      username: a.string(),
-      address: a.string(),
+      content: a.string(),
     })
     .authorization((allow) => [allow.publicApiKey()]),
   Users: a
